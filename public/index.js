51136f942a19481de20fb6cbae8c27151f32d352
Csumali <zmoly7@gmail.com>
2023-03-06 21:53:18 -0800

    Created non-functional login screen and allowed routing to it.

--- public/index.js
+++ public/index.js
@@ -4,3 +4,12 @@
 // It makes AJAX fetch calls to the API to satisfy different user interactions
 // with the MediaTrends application
 // -----------------------------------------------------------------------------
+
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.sendFile(__dirname + '/login.html');
+});
+
+module.exports = router;
