c62ebfa88ff5e11f5dee00d1306ec696a2c134c9
vhegde <vhegde@uw.edu>
2023-03-07 21:41:57 -0800

    Login completed

--- public/index.js
+++ public/index.js
@@ -4,12 +4,81 @@
 // It makes AJAX fetch calls to the API to satisfy different user interactions
 // with the MediaTrends application
 // -----------------------------------------------------------------------------
+"use strict";
+(function () {
+    const URL = "/verifyCredentials";
+    window.addEventListener("load", init);
 
-const express = require('express');
-const router = express.Router();
+    let username;
 
-router.get('/', (req, res) => {
-  res.sendFile(__dirname + '/login.html');
-});
+    /**
+     * Sets up page on load.
+     */
+    function init() {
+        console.log(sessionStorage.getItem('username'));
+        if(sessionStorage.getItem('username')) {
+            username = sessionStorage.getItem('username');
+        } else {
+            username = '';
+        }
+        loadUserData();
+    }
 
-module.exports = router;
+    function loadUserData() {
+        let heading = gen("h1");
+        heading.textContent = "welcome, " + username;
+        id("root").appendChild(heading);
+    }
+
+    /**
+    * Helper function to return the response's result text if successful, otherwise
+    * returns the rejected Promise result with an error status and corresponding text
+    * @param {object} res - response to check for success/error
+    * @return {object} - valid response if response was successful, otherwise rejected
+    *                    Promise result
+    */
+    async function statusCheck(res) {
+        if (!res.ok) {
+            throw new Error(await res.text());
+        }
+        return res;
+    }
+
+    /**
+     * Returns the element that has the ID attribute with the specified value.
+     * @param {string} name - element ID.
+     * @returns {object} DOM object associated with ID.
+     */
+    function id(name) {
+        return document.getElementById(name);
+    }
+
+
+
+    /**
+     * Returns first element matching selector.
+     * @param {string} selector - CSS query selector.
+     * @returns {object} - DOM object associated selector.
+     */
+    function qs(selector) {
+        return document.querySelector(selector);
+    }
+
+    /**
+     * Returns all element matching the selector.
+     * @param {string} selector - CSS query selector.
+     * @returns {array} - an array of DOM objects associated selector.
+     */
+    function qsa(selector) {
+        return document.querySelectorAll(selector);
+    }
+
+    /**
+     * creates and returns a new empty DOM node representing an element of that tagName type
+     * @param {string} tagName - HTML element type.
+     * @returns {object} - A new DOM object representing an element of that tagName type
+     */
+    function gen(tagName) {
+        return document.createElement(tagName);
+    }
+})();
--- public/login.js
+++ public/login.js
@@ -4,3 +4,68 @@
 // It makes AJAX fetch calls to the API to validate user credentials or create
 // new users in the database
 // -----------------------------------------------------------------------------
+"use strict";
+(function () {
+    const URL = "/verifyCredentials";
+    window.addEventListener("load", init);
+
+    /**
+     * Sets up page on load.
+     */
+    function init() {
+        sessionStorage.setItem('username', '');
+        id("login-form").addEventListener("submit", function(e) {
+            // Fires when submit event happens on form
+            // If we've gotten in here, all HTML5 validation checks have passed
+            e.preventDefault(); // prevent default behavior of submit (page refresh)
+            submitRequest(); // intended response function
+          });
+    }
+
+    function submitRequest() {
+        let user = id("username").value;
+        let pwd = id("password").value;
+
+        let param = new FormData();
+        param.append("username", user);
+        param.append("password", pwd);
+
+        fetch(URL, { method: "POST", body: param })
+            .then(statusCheck)
+            .then(res => res.text())
+            .then((response) => {
+                if(response === "verified") {
+                    sessionStorage.setItem('username', user);
+                    window.location.href = 'MediaTrends.html';
+                } else {
+                    id("username").value = "";
+                    id("password").value = "";
+                    id("invalid").classList.remove("hidden");
+                }
+             })
+            .catch(console.log);
+    }
+
+    /**
+ * Helper function to return the response's result text if successful, otherwise
+ * returns the rejected Promise result with an error status and corresponding text
+ * @param {object} res - response to check for success/error
+ * @return {object} - valid response if response was successful, otherwise rejected
+ *                    Promise result
+ */
+    async function statusCheck(res) {
+        if (!res.ok) {
+            throw new Error(await res.text());
+        }
+        return res;
+    }
+
+    /**
+     * Returns the element that has the ID attribute with the specified value.
+     * @param {string} name - element ID.
+     * @returns {object} DOM object associated with ID.
+     */
+    function id(name) {
+        return document.getElementById(name);
+    }
+})();
